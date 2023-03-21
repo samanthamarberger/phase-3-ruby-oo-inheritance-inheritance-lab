47887fcf9ddada8efcb5c4d2e2966b1d26a84573
samanthamarberger <slatham04@gmail.com>
2023-03-21 13:47:54 -0600

    done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(learned)
+        self.knowledge = self.knowledge.push(learned)
+    end
 end
